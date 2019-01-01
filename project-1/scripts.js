33d3200d70e57b5d7beeae84fd3375f848a155b0
mohamed <ma.camara@hotmail.com>
2019-01-01 18:43:15 -0500

    added new project folder

---
+++ project-1/scripts.js
@@ -0,0 +1,5 @@
+// alert('Hello Mohamed');
+// document.write("<h1>Welcome to JavaScript Basis</h1>");
+// alert('Thanks for visiting.');
+document.write("no it works!");
+console.log('Program complete');
