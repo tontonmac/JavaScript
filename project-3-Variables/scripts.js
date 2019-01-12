2d2d0713b7eb84ad120a1986f4d9db01b0efa013
mohamed <ma.camara@hotmail.com>
2019-01-11 19:11:06 -0500

    math objects

--- project-3-Variables/scripts.js
+++ project-3-Variables/scripts.js
@@ -35,3 +35,61 @@ message = 'Welcome to JavaScript Basics';
 // var firstName = prompt('what is your first name?');
 // var lastName = prompt('what is your last name?');
 // alert('The string \"' + (firstName +' '+ lastName).toUpperCase() + '\"'+ ' is ' + (firstName +' '+ lastName).length + ' characters long.');
+
+//PRACTICE VARIABLES
+
+// var secondsPerMin = 60;
+// var minsPerHour = 60;
+// var hoursPerDay = 24;
+// var daysPerWeek = 7;
+// var weeksPerYear = 52;
+// var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+
+// document.write('There are ' + secondsPerDay + ' seconds in a day </br>');
+
+// var yearsAlive = 18;
+// var secondsPerYear = secondsPerDay * daysPerWeek * weeksPerYear;
+
+// document.write('I have been alive  for ' + secondsPerYear * yearsAlive + ' seconds' );
+
+
+//STORY EXERCISE
+
+// var questions = 3;
+// var questionsLeft = ' [' + questions + ' questions left]';
+// var adjective = prompt('Please type an adjective' + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left]';
+// var verb = prompt('Please type a verb' + questionsLeft);
+// questions -= 1;
+// questionsLeft = ' [' + questions + ' questions left]';
+// var noun = prompt('Please type a noun' + questionsLeft);
+// alert('All done. Ready for the message?');
+// var sentence = "<h2>There once was a " + adjective;
+// sentence += ' programmer who wanted to use JavaScript to ' + verb;
+// sentence += ' the ' + noun + '.</h2>';
+// document.write(sentence);
+
+// MATH OBJECT
+// alert(Math.round(2.2));
+// alert(Math.round(4.6));
+
+// RANDOM - generate random numbe from 0 to 1
+// Math.random();
+
+// // get any number from 0 to 6
+// Math.random() * 6;
+
+// // get any number from 1 to 6 integer
+// var dieRoll = Math.floor(Math.random() * 6) + 1;
+// alert('You rolled ' + dieRoll);
+
+// CHALLENGE
+var rand = Math.random();
+var rand2 = Math.random();
+var userNum = prompt("Enter a number");
+// var userNum2 = prompt('Enter another number');
+var intNum = parseInt(userNum);
+// var intNum2 = parseInt(userNum2);
+var num = Math.floor(rand * intNum) + 1;
+document.write(num);
